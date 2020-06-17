ee07912e8c8e41261a58552aac9a155fafd76214
Leah F. South <leah.south@hdr.qut.edu.au>
2020-06-17 08:40:13 +0000

    version 2.1.0

--- R/zv.R
+++ R/zv.R
@@ -1,6 +1,7 @@
-zv <- function(integrand, samples, derivatives, log_weight, integrand_logged, obs_estim, polyorder, regul_reg, alpha_elnet, nfolds, apriori, intercept) {
+zv <- function(integrand, samples, derivatives, log_weights, integrand_logged, est_inds, polyorder, regul_reg, alpha_elnet, nfolds, apriori, intercept) {
 	nan_flag <- TRUE
 	N <- NROW(integrand)
+	N_expectations <- NCOL(integrand)
 	d <- NCOL(samples)
 	
 	if (d==1){
@@ -8,23 +9,27 @@ zv <- function(integrand, samples, derivatives, log_weight, integrand_logged, ob
 		derivatives <- array(derivatives,c(N,d))
 	}
 	
+	if (is.null(ncol(integrand))){
+		integrand <- matrix(integrand,ncol=1)
+	}
+	
 	if (integrand_logged){
-		max_integrand <- max(integrand)
-		integrand <- exp(integrand - max_integrand)
+		max_integrand <- apply(integrand,2,max)
+		integrand <- exp(integrand - matrix(1,nrow=N)%*%max_integrand)
 	}
 	
 	# Setting default weight
-	if (missing(log_weight)){
-		log_weight <- -log(N)*rep(1,N)
+	if (missing(log_weights)){
+		log_weights <- -log(N)*rep(1,N)
 	}
 	
 	# Here onwards depends on options
 	
-	if (is.null(obs_estim)){
-		obs_estim <- 1:N
-		obs_fit <- 1:N
+	if (is.null(est_inds)){
+		est_inds <- 1:N
+		eval_inds <- 1:N
 	} else {
-		obs_fit <- setdiff(1:N,obs_estim) 
+		eval_inds <- setdiff(1:N,est_inds) 
 	}
 	
 	# Using only relevant variables
@@ -32,28 +37,33 @@ zv <- function(integrand, samples, derivatives, log_weight, integrand_logged, ob
 	derivatives <- array(derivatives[,apriori],c(N,length(apriori)))
 	
 	# Splitting the data for fitting and estimation
-	log_weight_estim <- log_weight[obs_estim] - logsumexp(log_weight[obs_estim])
-	weight_estim <- exp(log_weight_estim)
-	y_estim <- integrand[obs_estim]
+	log_weights_eval <- log_weights[eval_inds] - logsumexp(log_weights[eval_inds])
+	weight_eval <- exp(log_weights_eval)
+	y_eval <- integrand[eval_inds,]
 	
 	# Get the data for fitting
-	log_weight_fit <- log_weight[obs_fit] - logsumexp(log_weight[obs_fit])
-	weight_fit <- exp(log_weight_fit)
-	y_fit <- integrand[obs_fit]
+	log_weights_estim <- log_weights[est_inds] - logsumexp(log_weights[est_inds])
+	weight_estim <- exp(log_weights_estim)
+	y_estim <- integrand[est_inds,]
+	
+	if (N_expectations==1){
+		y_eval <- matrix(y_eval,ncol=1)
+		y_estim <- matrix(y_estim,ncol=1)
+	}
 	
 	# Check if the polynomial order is 0.
 	if (polyorder == 0){
 		if (!integrand_logged){
-			expectation <- sum(weight_fit*y_fit)
+			expectation <- matrix(weight_estim,nrow=1)%*%y_estim
 		} else {
-			expectation <- logsumexp(log(y_fit) + log_weight_fit) + max_integrand
+			expectation <- apply(y_estim,2,function(x) logsumexp(log(x) + log_weights_estim)) + max_integrand
 		}
-		mse <-  mean((y_estim - expectation)^2)
+		mse <-  colMeans((y_eval - matrix(1,nrow=NROW(y_eval))%*%expectation)^2)
 		return(list(expectation = expectation, num_select = NaN, aic = NaN, bic = NaN, R2 = NaN, adjR2 = NaN, mse = mse,
-								integrand_logged = integrand_logged, obs_estim = obs_estim, polyorder = polyorder, regul_reg = regul_reg, alpha_elnet = alpha_elnet, nfolds = nfolds, apriori = apriori, intercept = intercept))
+								integrand_logged = integrand_logged, est_inds = est_inds, polyorder = polyorder, regul_reg = regul_reg, alpha_elnet = alpha_elnet, nfolds = nfolds, apriori = apriori, intercept = intercept))
 	} else{
-		X_estim <- getX(matrix(samples[obs_estim,],nrow=length(obs_estim),ncol=length(apriori)), matrix(derivatives[obs_estim,],nrow=length(obs_estim),ncol=length(apriori)), polyorder)
-		X_fit <- getX(samples[obs_fit,], derivatives[obs_fit,], polyorder)
+		X_eval <- getX(matrix(samples[eval_inds,],nrow=length(eval_inds),ncol=length(apriori)), matrix(derivatives[eval_inds,],nrow=length(eval_inds),ncol=length(apriori)), polyorder)
+		X_estim <- getX(samples[est_inds,], derivatives[est_inds,], polyorder)
 	}
 	
 	
@@ -62,64 +72,79 @@ zv <- function(integrand, samples, derivatives, log_weight, integrand_logged, ob
 	while (nan_flag){
 		
 		if (!intercept){
-			y_fit <- y_fit - sum(y_fit*weight_fit)
+			y_estim <- y_estim - matrix(1,nrow=NROW(y_estim))%*%weight_estim%*%y_estim
 		}
 		
-		
-		if (regul_reg){
+	  if (regul_reg){
+			num_select <- rep(NaN,NCOL(y_estim))
 			if (intercept){
-				fit <- glmnet(X_fit, y_fit, family = 'gaussian', alpha = alpha_elnet, weights = weight_fit, nlambda = 3)
-				mymax <- max(fit$lambda)
-				myseq <- c(0,exp(seq(log(mymax),min(-12,log(min(fit$lambda))),length.out = 99)))
-				fit <- cv.glmnet(X_fit, y_fit, family = 'gaussian', alpha = alpha_elnet, weights = weight_fit, lambda=myseq, nfolds = nfolds)
+				coefs <- matrix(NaN,nrow=NCOL(X_estim)+1,ncol=NCOL(y_estim))
+				for (j in 1:NCOL(y_estim)){
+					fit <- glmnet(X_estim, y_estim[,j], family = 'gaussian', alpha = alpha_elnet, weights = weight_estim, nlambda = 3)
+					mymax <- max(fit$lambda)
+					myseq <- c(0,exp(seq(log(mymax),min(-12,log(min(fit$lambda))),length.out = 99)))
+					fit <- cv.glmnet(X_estim, y_estim[,j], family = 'gaussian', alpha = alpha_elnet, weights = weight_estim, lambda=myseq, nfolds = nfolds)
+					
+					coefs[,j] <- coef(fit,s = "lambda.min")[,1]
+					num_select[j] <- unname(fit$nzero[which(fit$lambda == fit$lambda.min)])
+				}
 			} else{
-				fit <- glmnet(X_fit, y_fit, intercept = FALSE, family = 'gaussian', alpha = alpha_elnet, weights = weight_fit, nlambda = 3)
-				mymax <- max(fit$lambda)
-				myseq <- c(0,exp(seq(log(mymax),min(-12,log(min(fit$lambda))),length.out = 99)))
-				fit <- cv.glmnet(X_fit, y_fit, intercept = FALSE, family = 'gaussian', alpha = alpha_elnet, weights = weight_fit, lambda=myseq, nfolds = nfolds)
+				coefs <- matrix(NaN,nrow=NCOL(X_estim),ncol=NCOL(y_estim))
+				for (j in 1:NCOL(y_estim)){
+					fit <- glmnet(X_estim, y_estim[,j], intercept = FALSE, family = 'gaussian', alpha = alpha_elnet, weights = weight_estim, nlambda = 3)
+					mymax <- max(fit$lambda)
+					myseq <- c(0,exp(seq(log(mymax),min(-12,log(min(fit$lambda))),length.out = 99)))
+					fit <- cv.glmnet(X_estim, y_estim[,j], intercept = FALSE, family = 'gaussian', alpha = alpha_elnet, weights = weight_estim, lambda=myseq, nfolds = nfolds)
+					
+					coefs[,j] <- coef(fit,s = "lambda.min")
+					num_select[j] <- unname(fit$nzero[which(fit$lambda == fit$lambda.min)])
+				}
 			}
 			
-			coefs <- coef(fit,s = "lambda.min")
-			num_select <- unname(fit$nzero[which(fit$lambda == fit$lambda.min)])
+			
 			
 		} else {
 			if (intercept){
-				fit <- lm(y_fit ~ X_fit, weights = weight_fit)
+				fit <- lm(y_estim ~ X_estim, weights = weight_estim)
 			} else {
-				fit <- lm(y_fit ~ X_fit - 1, weights = weight_fit)
+				fit <- lm(y_estim ~ X_estim - 1, weights = weight_estim)
 			}
-			coefs <- coef(fit)
-			num_select <- NCOL(X_fit)
+			coefs <- coef(fit) # columns are different integrands
+			num_select <- rep(NCOL(X_estim),NCOL(y_estim))
+		}
+		
+		if (is.null(ncol(coefs))){
+			coefs <- matrix(coefs,ncol=1)
 		}
 		
 		if (intercept | regul_reg){
-			fitteds_estim <- X_estim%*%coefs[2:length(coefs)]
-			mse <- mean((y_estim - fitteds_estim - coefs[1])^2)
+			fitteds_eval <- X_eval%*%coefs[2:NROW(coefs),]
+			mse <- colMeans((y_eval - fitteds_eval - matrix(1,nrow=NROW(y_eval))%*%coefs[1,])^2)
 		} else {
-			fitteds_estim <- X_estim%*%coefs
-			mse <- mean((y_estim - fitteds_estim - sum(y_fit*weight_fit))^2)
+			fitteds_eval <- X_eval%*%coefs
+			mse <- colMeans((y_eval - fitteds_eval - matrix(1,nrow=NROW(y_eval))%*%weight_estim%*%y_estim)^2)
 		}
 		
 		
-		if (sum(is.na(fitteds_estim))>0){
-			fitteds_estim <- rep(0,NROW(y_estim))
+		if (any(is.na(fitteds_eval))){
+			fitteds_eval <- matrix(0,NROW(y_eval),NCOL(y_eval))
 		}
 		
-		integrand_new <- y_estim - fitteds_estim
+		integrand_new <- y_eval - fitteds_eval
 		
 		if (!integrand_logged){
-			expectation <- sum(weight_estim*integrand_new)
-		} else if(sum(integrand_new<0) != 0){
-			expectation <- log(sum(integrand_new*weight_estim)) + max_integrand
+			expectation <- weight_eval%*%integrand_new
+		} else if(any(integrand_new<0)){
+			expectation <- log(weight_eval%*%integrand_new) + max_integrand
 		} else {
-			expectation <- logsumexp(log(integrand_new) + log_weight_estim) + max_integrand
+			expectation <- apply(integrand_new,2,function(x) logsumexp(log(x) + log_weights_eval)) + max_integrand
 		}
 		
-		if (is.nan(expectation) & intercept){
+		if (any(is.nan(expectation)) & intercept){
 			intercept <- FALSE
 			print("NaN return. Rerunning now with a fixed intercept.")
 		}
-		else if (is.nan(expectation)){
+		else if (any(is.nan(expectation))){
 			nan_flag <- FALSE
 			print("Error - intercept was fixed and yet the expectation was NaN")
 		} else {
@@ -128,6 +153,8 @@ zv <- function(integrand, samples, derivatives, log_weight, integrand_logged, ob
 	}
 	
 	
-	return(list(expectation = expectation, num_select = num_select, mse = mse,
-							integrand_logged = integrand_logged, obs_estim = obs_estim, polyorder = polyorder, regul_reg = regul_reg, alpha_elnet = alpha_elnet, nfolds = nfolds, apriori = apriori, intercept = intercept))
+	return(list(expectation = expectation, num_select = num_select, mse = mse, coefs = coefs,
+							integrand_logged = integrand_logged, est_inds = est_inds,
+							polyorder = polyorder, regul_reg = regul_reg, alpha_elnet = alpha_elnet,
+							nfolds = nfolds, apriori = apriori, intercept = intercept))
 }
